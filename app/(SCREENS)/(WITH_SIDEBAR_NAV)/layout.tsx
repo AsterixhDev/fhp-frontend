5e97415f26e281f29d16a1f5f28474a0d23b5e4f
AsterixhDev <redoxgdd@gmail.com>
2025-12-10 12:21:22 +0100

    refactor: update ScreenWithSidebarLayout to use initialPathname prop and add CSS module declaration

--- app/(SCREENS)/(WITH_SIDEBAR_NAV)/layout.tsx
+++ app/(SCREENS)/(WITH_SIDEBAR_NAV)/layout.tsx
@@ -4,17 +4,13 @@ import PageScreen from "./_components/PageScreen";
 import Sidebar from "./_components/Sidebar";
 import { getPathnameOnServerSide } from "@/lib/getPathnameOnServerSide";
 
-interface ScreenWithSidebarLayoutProps extends PropsWithChildren {
-  pathname: string;
-}
-
 export default async function ScreenWithSidebarLayout({
   children,
-}: ScreenWithSidebarLayoutProps) {
+}: PropsWithChildren) {
   const pathname = await getPathnameOnServerSide()
   return (
     <div className="size-full flex flex-col-reverse md:grid grid-cols-1 md:gap-2 md:grid-cols-[2fr_5fr] md:max-w-6xl md:mx-auto md:p-4">
-      <Sidebar pathname={pathname} />
+      <Sidebar initialPathname={pathname} />
       <PageScreen>{children}</PageScreen>
     </div>
   );
